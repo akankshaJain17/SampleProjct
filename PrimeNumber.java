2c350356b65884d55aac3a173ce9f35a56209c99
Akanksha Jain <guptaakanksha1702@gmail.com>
2015-05-19 16:32:09 -0700

    New changes to Prime Number.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,22 +1,18 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Practice
 {
 	public static void main (String[] args)
 	{
 		int n=9;
 		boolean c = isPrime(n);
-			System.out.println(n + "is a Prime Number :" + c );
+		System.out.println(n + "is a Prime Number :" + c );
 	 
 	}
 	
 	public static boolean isPrime(int n)
 	{
-		for(int i = 2; i<=n-1; i++)
+		for(int i = 2; i<=n/2; i++)
 		{
 			if ((n % i) == 0)
 			{
@@ -27,20 +23,5 @@ class Practice
 		return true;
 	}
 	
-	public static void nPrimes(int n)
-	{
-		/*
-		
-		*/
-		for(int i = 2; i<=n-1; i++)
-		{
-			if ((n % i) == 0)
-			{
-				return false;
-			}
-		
-		}
-		return true;
-	}
 	
 }
